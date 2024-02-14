5cd35c1008faee6425b692879f32570557d82099
Ksenia Sidorenko <ksiu9939@gmail.com>
2024-02-14 21:19:15 +0300

    comment

--- main.go
+++ main.go
@@ -1,20 +1,9 @@
-//package main
-
-//import "fmt"
-
-// func main() {
-// fmt.Println("Hello,")
-// fmt.Print()
-// fmt.Println("Go!")
-// }
 package main
 
 import "fmt"
 
 func main() {
-	name := "Ksenia Sidorenko"
-	age := 24
-	fmt.Println("Имя и Фамилия -", name)
+	fmt.Println("Hello,")
 	fmt.Print()
-	fmt.Println("Возраст -", age)
+	fmt.Println("Go!")
 }
